d842fed661e025a1c658cecf610df00a9c3636ab
Liam <liamjnell@gmail.com>
2025-11-24 12:02:03 -0700

    Add <p> tag

--- src/frontend.php
+++ src/frontend.php
@@ -10,6 +10,7 @@ function render() {
         <div class="wrap">
             <h2>Hello World!</h2>
             <p>This is Liam's custom plugin for the internship</p>
+            <p>This is to create a pull request</p>
         </div>
 
     <?php
